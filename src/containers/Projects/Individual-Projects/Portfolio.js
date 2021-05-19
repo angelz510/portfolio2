566299674c35954a24a9782e98005d606bc0d026
Angel R <angelzrod4@gmail.com>
2021-05-19 15:32:36 -0700

    added new project

---
+++ src/containers/Projects/Individual-Projects/Portfolio.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import portfolio1 from '../../../assets/project-shots/portfolio1.png';
+import '../ProjectComponent.scss';
+
+const Portfolio = () => {
+  return (
+    <div className='project'>
+
+      <div className='project-photo'>
+        <img src={portfolio1} alt='portfolio'/>
+      </div>
+
+      <div className='project-info'>
+        <p className='project-name'>Portfolio</p>
+        <p className='project-techs'>Tools: React, Bootstrap, styled-components.</p>
+        <p className='project-description'>First sample portfolio built using React. 
+          <br/>Explored and practiced different packages; react-reveal, parallax, and Bootstrap.
+        </p>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Portfolio;
--- src/containers/Projects/ProjectContainer.js
+++ src/containers/Projects/ProjectContainer.js
@@ -4,11 +4,13 @@ import Budget from './Individual-Projects/Budget';
 import Hangry from './Individual-Projects/Hangry';
 import DreamLink from './Individual-Projects/DreamLink';
 import Rps from './Individual-Projects/Rps';
+import Portfolio from './Individual-Projects/Portfolio';
 
 const ProjectContainer = () => {
   return (
     <div className='ProjectContainer'>
       <Budget/>
+      <Portfolio/>
       <DreamLink/>
       <Hangry/>
       <Rps/>
